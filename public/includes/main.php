1d990836d831ff18c11022cb6947c14db13afd6a
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-07-08 15:20:37 -0300

    menu created

--- public/includes/main.php
+++ public/includes/main.php
@@ -8,9 +8,37 @@
                 </li>
                 <li>
                     <a href="#">Empresa</a>
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Empresa</a>
+                        </li>
+                        <li>
+                            <a href="#">Clientes</a>
+                        </li>
+                        <li>
+                            <a href="#">Contato</a>
+                            <ul>
+                                <li>
+                                    <a href="#">Home</a>
+                                </li>
+                                <li>
+                                    <a href="#">Empresa</a>
+                                </li>
+                                <li>
+                                    <a href="#">Clientes</a>
+                                </li>
+                                <li>
+                                    <a href="#">Contato</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
                 </li>
                 <li>
-                    <a href="#">Clients</a>
+                    <a href="#">Clientes</a>
                 </li>
                 <li>
                     <a href="#">Contato</a>
