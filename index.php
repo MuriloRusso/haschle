02beca943013b4d9ed68d8b30fa8831137e4af46
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-07-08 14:15:17 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php 
+        $title = "Home";
+        // $customCss = "/assets/custom.css";
+        include('./public/includes/head.php'); 
+    ?>
+</head>
+<body>
+    <h1>teste</h1>
+</body>
+</html>
