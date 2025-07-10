73fcb654fb041084df135b5116f40673bede96de
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-07-10 11:10:19 -0300

    Created featured boxes

--- index.php
+++ index.php
@@ -17,6 +17,34 @@
     </section>
     <section id="content">
 
+
+
+        <article id="featured-boxes" class="container paddingY">
+            <h2 class="text-uppercase text-center text-md-left">Boxes de Destaque</h2>
+
+            <div class="d-flex flex-wrap justify-content-center justify-content-md-between featured-boxes-container">
+                <div class="card">
+                    <img src="#" alt="">
+                    <a href="#">Um texto descritivo com link apenas</a>
+                </div>
+
+                <div class="card">
+                    <img src="#" alt="">
+                    <a href="#">Um texto descritivo com link apenas</a>
+                </div>
+
+                <div class="card">
+                    <img src="#" alt="">
+                    <a href="#">Um texto descritivo com link apenas</a>
+                </div>
+
+                <div class="card">
+                    <img src="#" alt="">
+                    <a href="#">Um texto descritivo com link apenas</a>
+                </div>
+            </div>
+        </article>
+
     </section>
 
     <?php include('./public/includes/footer.php');?>
