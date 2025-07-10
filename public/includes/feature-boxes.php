96a0ba26ddf4e02ecc303431a3fe983a8e4babf0
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-07-10 15:43:34 -0300

    wip

--- public/includes/feature-boxes.php
+++ public/includes/feature-boxes.php
@@ -3,22 +3,22 @@
 
   <div class="d-flex flex-wrap justify-content-center justify-content-md-between featured-boxes-container">
     <div class="card">
-      <img src="./public/src/img/banner/img-1.jpg" alt="">
+      <img src="./public/src/img/boxes/img-1.jpg" alt="">
       <a href="#">Como Atrair Mais Clientes com o Marketing Digital</a>
     </div>
 
     <div class="card">
-      <img src="./public/src/img/banner/img-2.jpg" alt="">
+      <img src="./public/src/img/boxes/img-2.jpg" alt="">
       <a href="#">A Importância de um Site Responsivo em 2025</a>
     </div>
 
     <div class="card">
-      <img src="./public/src/img/banner/img-3.jpg" alt="">
+      <img src="./public/src/img/boxes/img-3.jpg" alt="">
       <a href="#">Redes Sociais: Dicas Para Engajar Mais Seu Público</a>
     </div>
 
     <div class="card">
-      <img src="./public/src/img/banner/img-4.jpg" alt="">
+      <img src="./public/src/img/boxes/img-4.jpg" alt="">
       <a href="#">Por Que Investir em Tráfego Pago Ainda Vale a Pena</a>
     </div>
   </div>
